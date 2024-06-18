a390216e95833e3f030dce1b0f8c997d3ec360a3
junsoo <junsoo186@gmail.com>
2024-06-18 14:37:01 +0900

    처음

---
+++ src/dto/CardDTO.java
@@ -0,0 +1,19 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class CardDTO {
+	private int id;
+	private String url;
+	private String name;
+	private int price;
+}
