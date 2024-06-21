2896b20eb5401719854b3895f80aa2013b96acef
junsoo <junsoo186@gmail.com>
2024-06-21 12:20:06 +0900

    준수 경매 시간

--- src/manager/AuctionManager.java
+++ src/manager/AuctionManager.java
@@ -8,6 +8,7 @@ import dto.AuctionDTO;
 import dto.CardDTO;
 import dto.UserDTO;
 import lombok.Data;
+import swing.SellProductPanel; 
 
 @Data
 public class AuctionManager extends Thread {
@@ -16,34 +17,35 @@ public class AuctionManager extends Thread {
 	int room_Id;
 	// user
 	UserDTO user_DTO;
-	static AuctionDTO dto = new AuctionDTO();
+	AuctionDTO dto = new AuctionDTO();
+	SellProductPanel sellProductPanel;
 	CardDTO card_DTO;
 
 	// 시간 관련 변수
 	long current_time; // 로컬 컴퓨터 시간 변수 담을
-	int set_Hour; // 수정예정
-	int set_Min; // 수정예정
+	
+	int set_Hour ; // 수정예정
+	int set_Min ; // 수정예정
 	
 	LocalDateTime time = LocalDateTime.now();
-	LocalDateTime endTime = LocalDateTime.of(2024, 06, 21, set_Hour,set_Min, 0);
-
+	LocalDateTime endTime = LocalDateTime.of(2024, 06, 21, sellProductPanel.getThisHour(),
+			sellProductPanel.getThisMin(), 0);
 	// BID 관련 변수
 	int startBid;
 	int highbid;
 
-	public AuctionManager(int room, int set_Hour, int set_Min, int startBid,
+	public AuctionManager(int room, int startBid,
 			UserDTO user_DTO, CardDTO card_DTO,
 			Server mContext, int moreBid) {
 		this.room_Id = room;
-		this.set_Hour = set_Hour;
-		this.set_Min = set_Min;
+		sellProductPanel = new SellProductPanel(user_DTO);
+		endTime = LocalDateTime.of(2024, 06, 21,sellProductPanel.getThisHour(),
+				sellProductPanel.getThisMin(), 0);
 		this.startBid = startBid;
 		highbid = startBid;
-	
 		// DTO 
 		this.user_DTO = user_DTO;
 		this.card_DTO = card_DTO;
-
 		Vector<UserDTO> userID = new Vector<>();
 		start();
 	}
