2e5eb4bb1f9f22109f38a3765f1379e1005f203a
wonseok <misaka112@naver.com>
2024-06-25 10:13:49 +0900

    save 로그아웃

--- src/swing/LogOut.java
+++ src/swing/LogOut.java
@@ -2,23 +2,24 @@ package swing;
 
 import java.awt.Color;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class LogOut extends JFrame {
 	
 	JLabel logoutBack;
+	JButton logoutButton;
 	
 	public LogOut() {
-		
 		initData();
 		setInitLayout();
+		
 	}
 	
 	public void initData() {
 		logoutBack = new JLabel();
-	
-		
+		logoutButton = new JButton("LOGOUT");
 		
 	}
 	
@@ -32,7 +33,8 @@ public class LogOut extends JFrame {
 		setIconImage(java.awt.Toolkit.getDefaultToolkit().getImage("image/파비콘2.png"));
 		getContentPane().setBackground(Color.white);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		logoutButton.setBounds(90,90,100,30);
+		add(logoutButton);
 	}
 	
 	
