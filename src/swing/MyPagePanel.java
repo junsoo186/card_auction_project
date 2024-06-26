aada11d493eaff62cfa7f7215130fb63460d2c4d
eom song hyun <greenery3699@gmail.com>
2024-06-26 09:06:13 +0900

    마이페이지 동기화 오류 수정, 커밋:

--- src/swing/MyPagePanel.java
+++ src/swing/MyPagePanel.java
@@ -134,7 +134,6 @@ public class MyPagePanel extends JPanel {
 		// 회원 정보 수정
 		updateButton.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
-				System.out.println("회원정보수정 클릭");
 				try {
 					new CheckUserFrame(user,true,mContext);
 				} catch (SQLException e1) {
@@ -148,7 +147,6 @@ public class MyPagePanel extends JPanel {
 		// 진행중인 경매 이동
 		quitButton.addMouseListener(new MouseAdapter() {
 					public void mouseClicked(MouseEvent e) {
-						System.out.println("회원탈퇴 클릭");
 						try {
 							new CheckUserFrame(user,false,mContext);
 						} catch (SQLException e1) {
--- src/swing/UpdateUserFrame.java
+++ src/swing/UpdateUserFrame.java
@@ -63,7 +63,7 @@ public class UpdateUserFrame extends JFrame {
 
 		JLabel guidText = new JLabel(" 회원 정보 수정 ");
 		guidText.setFont(new Font("Freesentation 7 Bold", Font.BOLD, 20));
-		JLabel changeNicknameText = new JLabel(" 변경할 닉네임을 입력해주세요. (현재 닉네임 :"+user.getName()+ ")");
+		JLabel changeNicknameText = new JLabel(" 변경할 닉네임을 입력해주세요. (현재 닉네임 :"+user.getNickname()+ ")");
 		changeNicknameText.setFont(new Font("Freesentation 7 Bold", Font.BOLD, 14));
 		JLabel changePasswordText= new JLabel(" 비밀번호를 입력해주세요. (현재 비밀번호 :"+user.getPassword()+" )");
 		changePasswordText.setFont(new Font("Freesentation 7 Bold", Font.BOLD, 14));
@@ -106,8 +106,6 @@ public class UpdateUserFrame extends JFrame {
 					JOptionPane.showMessageDialog(null,"잘못된 정보를 입력했습니다. 다시 입력해주세요.");
 				}else {
 					socket.sendOrder("updateUserInfo#"+newNick+"#"+newPass+"#"+userName);
-					System.out.println("새로운 닉네임 : " + newNick);
-					System.out.println("새로운 패스워드 : " + newPass);
 					JOptionPane.showMessageDialog(null,"회원 정보가 성공적으로 수정되었습니다.");
 					try {
 						Thread.sleep(500);
